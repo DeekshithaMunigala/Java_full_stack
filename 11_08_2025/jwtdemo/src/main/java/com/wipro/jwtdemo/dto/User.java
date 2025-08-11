05a490487752aad8067d9d7dd6c2544290371660
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-11 13:03:31 +0530

    Updated

---
+++ 11_08_2025/jwtdemo/src/main/java/com/wipro/jwtdemo/dto/User.java
@@ -0,0 +1,30 @@
+package com.wipro.jwtdemo.dto;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private String token;
+	
+	public String getName() {
+		return username;
+	}
+	public void setName(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+
+}
---
+++ 11_08_2025/jwtdemo/src/main/java/com/wipro/jwtdemo/util/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.wipro.jwtdemo.util;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+	
+	private final String SECRET_KEY = "secretKeyJWT";
+
+    public String generateToken(String username,  List<String> authorities) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("authorities", authorities)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) 
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return (username.equals(extractUsername(token)) && !isTokenExpired(token));
+    }
+
+}
