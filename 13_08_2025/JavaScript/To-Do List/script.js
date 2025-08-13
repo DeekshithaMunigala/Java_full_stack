f4694e6e69a7ef1c8b1cb5f46caeaf6809fa5e1b
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-13 15:57:36 +0530

    Updated

---
+++ 13_08_2025/JavaScript/To-Do List/script.js
@@ -0,0 +1,20 @@
+const input = document.getElementById("task");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("task-list");
+
+addBtn.addEventListener("click", function () {
+  const task = input.value;
+
+  const li = document.createElement("li");
+  li.innerText = task;
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "Delete";
+
+  delBtn.addEventListener("click", function () {
+    taskList.removeChild(li);
+  });
+
+  li.appendChild(delBtn);
+  taskList.appendChild(li);
+});
