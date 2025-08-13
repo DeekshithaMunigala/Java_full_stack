2243904fd5988a866ddf68241de4ffc20b7c537d
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-13 13:54:33 +0530

    Updated

--- 13_08_2025/JavaScript/reduce.js
+++ 13_08_2025/JavaScript/reduce.js
@@ -30,3 +30,62 @@ let totalSalary = employees
   .filter((emp) => emp.role === "manager")
   .reduce((total, emp) => total + emp.empSalary, 0);
 console.log(`The total salary drawn by all managers is : ${totalSalary}`);
+
+// Exercise-9
+// persons list is already in the hello.js so not needed to include here we can still access it
+
+// for (let i = 0; i < persons.length; i++) {
+//   if (persons[i].age > 18) {
+//     persons[i].ageStatus = "Adult";
+//   } else {
+//     persons[i].ageStatus = "Minor";
+//   }
+// }
+
+// console.log(persons);
+
+// Exercise-10
+let newPersonsList = persons.map((person) => {
+  if (person.age > 18) {
+    person.ageStatus = "Adult";
+  } else {
+    person.ageStatus = "Minor";
+  }
+  return person;
+});
+console.log(newPersonsList);
+
+// Exercise-11
+let date = new Date(2030, 7, 13);
+
+let weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let dayName = weekDays[date.getDay()];
+console.log(dayName);
+console.log(date.getDay());
+
+// Exercise-12
+
+let name = "madam";
+
+let palindrome = name.split("").reverse().join("");
+
+if (name === palindrome) {
+  console.log(`The string ${name} is a palindrome`);
+} else {
+  console.log(`The string ${name} is not a palindrome`);
+}
+
+let reverse = "";
+for (let i = name.length; i >= 0; i--) {
+  reverse += name[i];
+}
+console.log(reverse);
