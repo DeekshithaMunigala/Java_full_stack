1996390f31eadf848f708b3213c438251b50a2ce
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-18 15:38:06 +0530

    Updated

---
+++ 17_08_2025/Angular/secondApp/src/app/listcomp/listcomp.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Conditioncomp } from '../conditioncomp/conditioncomp';
+import { Switchdemo } from '../switchdemo/switchdemo';
+import { FilteredNames } from '../filtered-names/filtered-names';
+
+@Component({
+  selector: 'app-listcomp',
+  imports: [Conditioncomp, Switchdemo, FilteredNames],
+  templateUrl: './listcomp.html',
+  styleUrl: './listcomp.css',
+})
+export class Listcomp {
+  namesList: string[] = ['Deekshitha', 'Amit', 'Ryan', 'Jack', 'Jhon'];
+}
--- 17_08_2025/Angular/secondApp/src/main.ts
+++ 17_08_2025/Angular/secondApp/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Listcomp } from './app/listcomp/listcomp';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(Listcomp, appConfig).catch((err) => console.error(err));
