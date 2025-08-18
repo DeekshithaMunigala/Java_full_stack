d75e76dc3c731994d9f29272f842e3b8dff15755
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-18 17:38:42 +0530

    Updated

--- 17_08_2025/Angular/secondApp/src/app/listcomp/listcomp.ts
+++ 17_08_2025/Angular/secondApp/src/app/listcomp/listcomp.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Conditioncomp } from '../conditioncomp/conditioncomp';
 import { Switchdemo } from '../switchdemo/switchdemo';
 import { FilteredNames } from '../filtered-names/filtered-names';
+import { Home } from '../home/home';
 
 @Component({
   selector: 'app-listcomp',
-  imports: [Conditioncomp, Switchdemo, FilteredNames],
+  imports: [Conditioncomp, Switchdemo, FilteredNames, Home],
   templateUrl: './listcomp.html',
   styleUrl: './listcomp.css',
 })
--- 17_08_2025/Angular/secondApp/src/app/switchdemo/switchdemo.ts
+++ 17_08_2025/Angular/secondApp/src/app/switchdemo/switchdemo.ts
@@ -8,5 +8,5 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './switchdemo.css',
 })
 export class Switchdemo {
-  color: string = 'red';
+  color: string = 'default';
 }
