18c8ffbdfa099fb99e5c67841c73104ed7b80803
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-18 12:32:50 +0530

    Updated

--- 17_08_2025/Angular/secondApp/src/app/app.ts
+++ 17_08_2025/Angular/secondApp/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('secondApp');
   name: string = 'Deekshitha';
+
+  greet() {
+    console.log(`${this.name} says Hello!`);
+  }
 }
