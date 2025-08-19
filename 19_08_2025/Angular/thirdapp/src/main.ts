c0087871bb1e3be88d84e6a359e553e5b19f923a
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-19 12:42:20 +0530

    Updated

--- 19_08_2025/Angular/thirdapp/src/main.ts
+++ 19_08_2025/Angular/thirdapp/src/main.ts
@@ -3,4 +3,4 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { Ex1home } from './app/ex1home/ex1home';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(Ex1home, appConfig).catch((err) => console.error(err));
