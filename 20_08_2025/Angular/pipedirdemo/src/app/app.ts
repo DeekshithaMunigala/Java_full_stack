1eb62cd76ebea1ada40484c3c4c93463ab8d1bd3
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-20 15:51:26 +0530

    Updated

--- 20_08_2025/Angular/pipedirdemo/src/app/app.ts
+++ 20_08_2025/Angular/pipedirdemo/src/app/app.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CentitofahrenPipe } from './centitofahren-pipe';
+import { Highlightandstrikedir } from './highlightandstrikedir';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CommonModule, UpperCasePipe, DatePipe, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    UpperCasePipe,
+    DatePipe,
+    ReactiveFormsModule,
+    CentitofahrenPipe,
+    Highlightandstrikedir,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -18,4 +28,6 @@ export class App {
   dateControl = new FormControl();
 
   date: string | null = null;
+
+  centigrade: number = 20;
 }
