c2852b934e43ea29ca96b75e94161f3108bbd8b5
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-21 17:50:09 +0530

    Updated

--- 21_08_2025/Angular/fourthapp/src/app/vechilecomp/vechilecomp.ts
+++ 21_08_2025/Angular/fourthapp/src/app/vechilecomp/vechilecomp.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Ivechile } from '../ivechile';
 import { Vechileservice } from '../vechileservice';
+import { ChangeDetectorRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vechilecomp',
@@ -9,19 +12,52 @@ import { Vechileservice } from '../vechileservice';
   styleUrl: './vechilecomp.css',
 })
 export class Vechilecomp {
-  constructor(private vechileService: Vechileservice) {}
+  constructor(private vechileService: Vechileservice, private cdr: ChangeDetectorRef) {}
 
   vechiles: Ivechile[] = [];
+
+  vechileEdit: Ivechile = {
+    id: '',
+    make: '',
+    model: '',
+    fuelType: '',
+    price: 0,
+  };
+  vechileAdd: Ivechile = {
+    id: '',
+    make: '',
+    model: '',
+    fuelType: '',
+    price: 0,
+  };
 
   ngOnInit() {
     this.vechileService.getVechiles().subscribe(
       (data: Ivechile[]) => {
         this.vechiles = data;
+        this.cdr.detectChanges();
         console.log(data);
       },
       (error) => {
-        console.log('Error fetching vechile : ', error);
+        console.error('Error fetching vechile : ', error);
       }
     );
   }
+
+  edit(vechileId: string) {
+    console.log(vechileId);
+    this.vechileService.getVechileById(vechileId).subscribe(
+      (vechile: Ivechile) => {
+        console.log('user details', vechile);
+        this.vechileEdit = vechile;
+        console.log('user details after editing', vechile);
+        this.cdr.detectChanges();
+      },
+      (error) => {
+        console.error('Error fectching user by ID: ', error);
+      }
+    );
+  }
+
+  delete(vechileId: string) {}
 }
--- 21_08_2025/Angular/fourthapp/src/app/vechileservice.ts
+++ 21_08_2025/Angular/fourthapp/src/app/vechileservice.ts
@@ -12,4 +12,26 @@ export class Vechileservice {
   getVechiles(): Observable<Ivechile[]> {
     return this.http.get<Ivechile[]>('http://localhost:3000/vechile');
   }
+
+  getVechileById(vechileId: string): Observable<Ivechile> {
+    return this.http.get<Ivechile>('http://localhost:3000/vechile/' + vechileId);
+  }
+
+  updateVechile(vechile: Ivechile): Observable<Ivechile> {
+    return this.http.put<Ivechile>(`http://localhost:3000/vechile/${vechile.id}`, vechile);
+  }
+
+  deleteVechile(vechileId: string): Observable<void> {
+    return this.http.delete<void>(`http://localhost:3000/vechile/${vechileId}`);
+  }
+
+  saveVechile(vechile: Ivechile): Observable<Ivechile> {
+    let vechileSave: Partial<Ivechile> = {
+      make: vechile.make,
+      model: vechile.model,
+      fuelType: vechile.fuelType,
+      price: vechile.price,
+    };
+    return this.http.post<Ivechile>('http://localhost:3000/vechile', vechile);
+  }
 }
