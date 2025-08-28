cfe8b1b08f8f40abdbb4a4e2ffc57836be79d656
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2025-08-28 18:11:31 +0530

    Updated All

--- capstone_project/backend/userms/src/main/java/com/wipro/userms/UsermsApplication.java
+++ capstone_project/backend/userms/src/main/java/com/wipro/userms/UsermsApplication.java
@@ -2,8 +2,10 @@ package com.wipro.userms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsermsApplication {
 
 	public static void main(String[] args) {
